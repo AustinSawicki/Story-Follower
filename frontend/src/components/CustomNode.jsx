7221807810183da23105dac24c352bc73f61bcf2
Austin Sawicki <sawickiaustin@yahoo.com>
2024-07-18 13:41:51 -0400

    couldn't drag on the short description area on nodes in the story tree. Now works.

--- frontend/src/components/CustomNode.jsx
+++ frontend/src/components/CustomNode.jsx
@@ -4,40 +4,17 @@ import Settings from './Settings';
 import { RiSettings4Fill } from "react-icons/ri";
 
 const CustomNode = ({ data }) => {
-    const textareaRef = useRef(null);
-    const nodeRef = useRef(null);
-
-    useEffect(() => {
-        const adjustHeight = () => {
-            if (textareaRef.current) {
-                textareaRef.current.style.height = 'auto';
-                textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
-            }
-            if (nodeRef.current) {
-                nodeRef.current.style.height = 'auto';
-                nodeRef.current.style.height = `${textareaRef.current.scrollHeight + 40}px`; // Adjust 50px based on other contents height
-            }
-        };
-        
-        adjustHeight();
-    }, [data.description]);
-
     return (
         <div
-            ref={nodeRef}
-            className="bg-theme p-2 rounded border-2 border-button relative h-auto"
-            style={{ width: '200px' }}
+            className="bg-theme p-2 rounded border-2 border-button relative"
+            style={{ width: '200px', height: '200px' }}
         >
             <p className="font-bold overflow-hidden text-ellipsis whitespace-nowrap text-center">
                 {data.label}
             </p>
-            <textarea
-                ref={textareaRef}
-                className="w-full h-auto font-semibold text-sm rounded resize-none bg-theme"
-                value={data.description || ""}
-                disabled
-                placeholder=""
-            />
+            <div className="mt-2 short-description-box">
+                    <p>{data.description || ""}</p>
+            </div>
             <RiSettings4Fill
                 onClick={data.onClick}
                 className="absolute font-bold text-sm top-3 right-2 text-button rounded-full hover:text-button-dark cursor-pointer"
