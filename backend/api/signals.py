43c9b0ac26c19550132b390a2c0c19bb6e2a77f9
Austin Sawicki <sawickiaustin@yahoo.com>
2024-07-21 19:06:06 -0400

    database image changes

--- backend/api/signals.py
+++ backend/api/signals.py
@@ -3,6 +3,8 @@ import shutil
 from django.db.models.signals import pre_delete, post_save
 from django.dispatch import receiver
 from .models import Story, CharacterCard, CustomUser, Theme
+import boto3
+from django.conf import settings
 from django.core.files.storage import default_storage
 
 default_themes = [
@@ -38,6 +40,22 @@ def delete_story_images(sender, instance, **kwargs):
     for character in instance.character_cards.all():
         if character.image:
             default_storage.delete(character.image.name)
+    
+    s3_client = boto3.client(
+        's3',
+        region_name=settings.AWS_S3_REGION_NAME,
+        endpoint_url=settings.AWS_S3_ENDPOINT_URL,
+        aws_access_key_id=settings.AWS_S3_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_S3_SECRET_ACCESS_KEY,
+    )
+
+    bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+    prefix = f'story_images/{instance.id}/'
+
+    response = s3_client.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+    if 'Contents' in response:
+        for obj in response['Contents']:
+            s3_client.delete_object(Bucket=bucket_name, Key=obj['Key'])
 
 @receiver(pre_delete, sender=CharacterCard)
 def delete_character_images(sender, instance, **kwargs):
