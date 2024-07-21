bc05644b56ad9e4249a5a4a070acb819bf9d566c
Austin Sawicki <sawickiaustin@yahoo.com>
2024-07-21 16:19:25 -0400

    changed story tree node size

--- frontend/src/components/CustomNode.jsx
+++ frontend/src/components/CustomNode.jsx
@@ -7,7 +7,7 @@ const CustomNode = ({ data }) => {
     return (
         <div
             className="bg-theme p-2 rounded border-2 border-button relative"
-            style={{ width: '200px', height: '200px' }}
+            style={{ width: '250px', height: '220px' }}
         >
             <p className="font-bold overflow-hidden text-ellipsis whitespace-nowrap text-center">
                 {data.label}
