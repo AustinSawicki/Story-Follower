3e7cce6e08012955d09f96a99824d92bb1532d6a
Austin Sawicki <sawickiaustin@yahoo.com>
2024-07-21 17:40:39 -0400

    database image delete settings

--- backend/api/signals.py
+++ backend/api/signals.py
@@ -28,27 +28,22 @@ def create_default_themes(sender, instance, created, **kwargs):
 @receiver(pre_delete, sender=Story)
 def delete_story_images(sender, instance, **kwargs):
     # Delete story images
+    print("DELETE STORY IMAGES")
     if instance.image:
-        default_storage.delete(instance.image.path)
+        default_storage.delete(instance.image.name)
     if instance.banner:
-        default_storage.delete(instance.banner.path)
+        default_storage.delete(instance.banner.name)
     
     # Delete character images related to the story
-    character_image_paths = []
     for character in instance.character_cards.all():
         if character.image:
-            character_image_paths.append(character.image.path)
-            default_storage.delete(character.image.path)
-    
-    # Remove the directory if it exists
-    story_folder = os.path.join('media', 'story_images', str(instance.id))
-    if os.path.exists(story_folder) and not os.listdir(story_folder):
-        shutil.rmtree(story_folder)
+            default_storage.delete(character.image.name)
 
 @receiver(pre_delete, sender=CharacterCard)
 def delete_character_images(sender, instance, **kwargs):
+    print("DELETE CHARACTER IMAGES")
     if instance.image:
-        default_storage.delete(instance.image.path)
+        default_storage.delete(instance.image.name)
     
     # Check if the story folder should be deleted
     story_folder = os.path.join('media', 'story_images', str(instance.story.id), 'characters')
